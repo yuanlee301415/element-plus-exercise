6d3b9cd28f9eaab022a28fc4363b6e25435e3a6f
yuanlee <yuanlee@163.com>
2023-03-01 17:08:38 +0800

    Test: Statistic

---
+++ src/views/statistic.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>prefix / suffix</h2>
+  <el-statistic :value="25600" @change="change">
+    <template #title><b>商品价格</b></template>
+    <template #prefix>￥</template>
+    <template #suffix>元</template>
+  </el-statistic>
+
+  <el-countdown
+    :value="new Date().getTime() + 1000 * 60"
+    @change="change"
+  >
+
+  </el-countdown>
+</template>
+
+<script lang="ts" setup>
+function change(val: number) {
+  console.log('change>val:', val)
+}
+
+</script>
