6fbcfce58fdc22c460697eee8e7eae8b784c9bbb
yuanlee <yuanlee@163.com>
2023-03-16 16:52:11 +0800

    test: Button expose

--- src/views/Button.vue
+++ src/views/Button.vue
@@ -1,39 +1,38 @@
 <template>
   <h1>Basic</h1>
-  <el-button>Button</el-button>
+  <el-button ref="basicBtnRef">Button</el-button>
 
-  <hr>
+  <hr />
   <h1>分组</h1>
   <el-button-group type="success" size="large">
     <el-button>Button</el-button>
     <el-button>Button</el-button>
   </el-button-group>
-  <hr>
+  <hr />
   <el-button-group type="success" size="large">
     <el-button type="warning">Button</el-button>
     <el-button size="small">Button</el-button>
   </el-button-group>
-  <hr>
+  <hr />
   <el-button-group type="success" size="large">
     <button>1</button>
     <el-button type="warning">Button</el-button>
-    <input type="text">
+    <input type="text" />
     <el-button size="small">Button</el-button>
   </el-button-group>
 
-  <hr>
+  <hr />
   <h1>切换选择组件属性</h1>
-  <select v-model="type">
+  <select v-model="type" @change="onChangeType">
     <option value="">--Change type--</option>
     <option value="primary">Primary</option>
     <option value="success">Success</option>
   </select>
   <el-button-group :type="type" size="large">
-    <el-button>Button</el-button>
-    <el-button>Button</el-button>
+    <el-button ref="typeBtnRef">Button</el-button>
   </el-button-group>
 
-  <hr>
+  <hr />
   <select v-model="size">
     <option value="">--Change size--</option>
     <option value="small">Small</option>
@@ -46,8 +45,23 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
+import type { ButtonInstance } from "element-plus";
+import { onMounted, ref } from "vue";
 
-const type = ref('')
-const size = ref('')
+const type = ref("");
+const size = ref("");
+const basicBtnRef = ref<ButtonInstance>();
+const typeBtnRef = ref<ButtonInstance>();
+
+onMounted(() => {
+  console.log("basicBtnRef:", basicBtnRef.value);
+  console.log("button>ref:", basicBtnRef.value?.ref);
+  console.log("button>type:", typeBtnRef.value?.type);
+});
+
+function onChangeType() {
+  setTimeout(() => {
+    console.log("onChangeType>type:", typeBtnRef.value?.type);
+  })
+}
 </script>
